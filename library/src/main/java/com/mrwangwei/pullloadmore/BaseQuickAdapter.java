d0ac111b2e3fe8ddc6da50086bb8aa7e98d1be02
93987169@qq.com <wangwei>
2019-04-23 16:39:53 +0800

    ww

--- library/src/main/java/com/mrwangwei/pullloadmore/BaseQuickAdapter.java
+++ library/src/main/java/com/mrwangwei/pullloadmore/BaseQuickAdapter.java
@@ -1,7 +1,6 @@
 package com.mrwangwei.pullloadmore;
 
 import android.animation.Animator;
-import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.IdRes;
 import android.support.annotation.IntDef;
