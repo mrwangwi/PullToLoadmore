cd5359df225c6c47c33e22dfd443d4b8808f14a1
admin <wangwei>
2019-10-30 18:37:24 +0800

    ww

---
+++ library/src/main/java/com/mrwangwei/pullloadmore/BaseQuickSimpleAdapter.java
@@ -0,0 +1,11 @@
+package com.mrwangwei.pullloadmore;
+
+import android.support.v7.widget.RecyclerView;
+
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public abstract class BaseQuickSimpleAdapter<T, K extends BaseViewHolder> extends RecyclerView.Adapter<K> {
+
+}
